392c93c998d6f7f9f22e947eb778c9a5c50868a3
eliasawess <eliasawess@gmail.com>
2023-05-23 14:52:05 +0300

    C# http requests

---
+++ WindowsFormsApp1/APIRequestModule.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Reflection;
+
+namespace WindowsFormsApp1
+{
+    class APIRequestModule
+    {
+        private static string authToken = "";
+
+        private static HttpClient client = new HttpClient { BaseAddress = new Uri("http://localhost:8000/") };
+
+        public static string getAuthToken()
+        {
+            return authToken;
+        }
+
+        public static void setAuthToken(string token)
+        {
+            authToken = token;
+        }
+
+        public static async Task<JsonElement> sendGetRequest(string uri, bool authorized = false)
+        {
+            // Create HttpRequestMessage
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            // Set headers
+            if (authorized)
+            {
+                request.Headers.Add("Authorization", $"Token {authToken}");
+            }
+
+            // Send the request
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            // Read the response body
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Parse the JSON response body
+            JsonElement parsedResponse = ParseJson(responseBody);
+
+            // Display response status code, body, and parsed JSON
+            Console.WriteLine("Response Status Code: " + response.StatusCode);
+            Console.WriteLine("Response Body: " + responseBody);
+            Console.WriteLine("Parsed JSON: " + parsedResponse);
+
+            return parsedResponse;
+        }
+
+        public static async Task<JsonElement> sendPostRequest(string uri, object body, bool authorized = false)
+        {
+            // Convert the object to JSON string
+            string requestBody = ConvertToJson(body);
+
+            // Create HttpContent from the request body
+            var httpContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            // Set headers
+            if (authorized)
+            {
+                httpContent.Headers.Add("Authorization", $"Token {authToken}");
+            }
+
+            // Send the POST request
+            HttpResponseMessage response = await client.PostAsync(uri, httpContent);
+
+            // Read the response body
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Parse the JSON response body
+            JsonElement parsedResponse = ParseJson(responseBody);
+
+            // Display response status code, body, and parsed JSON
+            Console.WriteLine("Response Status Code: " + response.StatusCode);
+            Console.WriteLine("Response Body: " + responseBody);
+            Console.WriteLine("Parsed JSON: " + parsedResponse);
+
+            return parsedResponse;
+        }
+
+        public static async Task<JsonElement> sendPutRequest(string uri, object body, bool authorized = false)
+        {
+            // Convert the object to JSON string
+            string requestBody = ConvertToJson(body);
+
+            // Create HttpContent from the request body
+            var httpContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            // Set headers
+            if (authorized)
+            {
+                httpContent.Headers.Add("Authorization", $"Token {authToken}");
+            }
+
+            // Send the POST request
+            HttpResponseMessage response = await client.PutAsync(uri, httpContent);
+
+            // Read the response body
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Parse the JSON response body
+            JsonElement parsedResponse = ParseJson(responseBody);
+
+            // Display response status code, body, and parsed JSON
+            Console.WriteLine("Response Status Code: " + response.StatusCode);
+            Console.WriteLine("Response Body: " + responseBody);
+            Console.WriteLine("Parsed JSON: " + parsedResponse);
+
+            return parsedResponse;
+        }
+
+        public static async Task<JsonElement> sendDeleteRequest(string uri, object body, bool authorized = false)
+        {
+            // Create HttpRequestMessage
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, uri);
+
+            // Convert the object to JSON string
+            string requestBody = ConvertToJson(body);
+
+            // Create HttpContent from the request body
+            var httpContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            // Set the request content
+            request.Content = httpContent;
+
+            // Set headers
+            if (authorized)
+            {
+                request.Headers.Add("Authorization", $"Token {authToken}");
+            }
+
+            // Send the request
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            // Read the response body
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Parse the JSON response body
+            JsonElement parsedResponse = ParseJson(responseBody);
+
+            // Display response status code, body, and parsed JSON
+            Console.WriteLine("Response Status Code: " + response.StatusCode);
+            Console.WriteLine("Response Body: " + responseBody);
+            Console.WriteLine("Parsed JSON: " + parsedResponse);
+
+            return parsedResponse;
+        }
+
+        public static string ConvertToJson(object data)
+        {
+            // Convert object to JSON string using System.Text.Json
+            string jsonString = JsonSerializer.Serialize(data);
+            return jsonString;
+        }
+
+        public static JsonElement ParseJson(string json)
+        {
+            // Parse JSON logic goes here
+            // Modify this method according to your JSON parsing requirements
+            // It should return the parsed object
+            return JsonSerializer.Deserialize<JsonElement>(json);
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/LoginForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.loginPictureBox = new System.Windows.Forms.PictureBox();
+            this.usernamePanel = new System.Windows.Forms.Panel();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordPanel = new System.Windows.Forms.Panel();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginBusttonPanel = new System.Windows.Forms.Panel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.loginPictureBox)).BeginInit();
+            this.usernamePanel.SuspendLayout();
+            this.passwordPanel.SuspendLayout();
+            this.loginBusttonPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.ForeColor = System.Drawing.SystemColors.Window;
+            this.welcomeLabel.Location = new System.Drawing.Point(631, 163);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(280, 69);
+            this.welcomeLabel.TabIndex = 0;
+            this.welcomeLabel.Text = "Welcome";
+            this.welcomeLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // loginPictureBox
+            // 
+            this.loginPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.loginPictureBox.Image = global::WindowsFormsApp1.Properties.Resources.keyhole;
+            this.loginPictureBox.Location = new System.Drawing.Point(131, 26);
+            this.loginPictureBox.Name = "loginPictureBox";
+            this.loginPictureBox.Size = new System.Drawing.Size(201, 175);
+            this.loginPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.loginPictureBox.TabIndex = 1;
+            this.loginPictureBox.TabStop = false;
+            this.loginPictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // usernamePanel
+            // 
+            this.usernamePanel.BackColor = System.Drawing.Color.Transparent;
+            this.usernamePanel.Controls.Add(this.usernameLabel);
+            this.usernamePanel.Controls.Add(this.usernameTextBox);
+            this.usernamePanel.Location = new System.Drawing.Point(32, 230);
+            this.usernamePanel.Name = "usernamePanel";
+            this.usernamePanel.Size = new System.Drawing.Size(398, 126);
+            this.usernamePanel.TabIndex = 2;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(6, 22);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(150, 36);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Username";
+            this.usernameLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTextBox.Location = new System.Drawing.Point(12, 67);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(360, 34);
+            this.usernameTextBox.TabIndex = 0;
+            // 
+            // passwordPanel
+            // 
+            this.passwordPanel.BackColor = System.Drawing.Color.Transparent;
+            this.passwordPanel.Controls.Add(this.passwordLabel);
+            this.passwordPanel.Controls.Add(this.passwordTextBox);
+            this.passwordPanel.Location = new System.Drawing.Point(32, 374);
+            this.passwordPanel.Name = "passwordPanel";
+            this.passwordPanel.Size = new System.Drawing.Size(398, 126);
+            this.passwordPanel.TabIndex = 3;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(6, 22);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(147, 36);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password";
+            this.passwordLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(12, 67);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(360, 34);
+            this.passwordTextBox.TabIndex = 0;
+            // 
+            // loginBusttonPanel
+            // 
+            this.loginBusttonPanel.BackColor = System.Drawing.Color.Transparent;
+            this.loginBusttonPanel.Controls.Add(this.messageLabel);
+            this.loginBusttonPanel.Controls.Add(this.loginButton);
+            this.loginBusttonPanel.Location = new System.Drawing.Point(32, 506);
+            this.loginBusttonPanel.Name = "loginBusttonPanel";
+            this.loginBusttonPanel.Size = new System.Drawing.Size(398, 132);
+            this.loginBusttonPanel.TabIndex = 4;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(72, 66);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(214, 63);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Log in";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            this.loginButton.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.loginButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(948, 12);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(35, 35);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "x";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.BackColor = System.Drawing.Color.Transparent;
+            this.minimizeButton.FlatAppearance.BorderSize = 0;
+            this.minimizeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan;
+            this.minimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimizeButton.Location = new System.Drawing.Point(907, 12);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(35, 35);
+            this.minimizeButton.TabIndex = 6;
+            this.minimizeButton.Text = "-";
+            this.minimizeButton.UseVisualStyleBackColor = false;
+            this.minimizeButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.ForeColor = System.Drawing.Color.Red;
+            this.messageLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.messageLabel.Location = new System.Drawing.Point(7, 13);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(0, 25);
+            this.messageLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.images1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(998, 650);
+            this.Controls.Add(this.minimizeButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.loginBusttonPanel);
+            this.Controls.Add(this.passwordPanel);
+            this.Controls.Add(this.usernamePanel);
+            this.Controls.Add(this.loginPictureBox);
+            this.Controls.Add(this.welcomeLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loginPictureBox)).EndInit();
+            this.usernamePanel.ResumeLayout(false);
+            this.usernamePanel.PerformLayout();
+            this.passwordPanel.ResumeLayout(false);
+            this.passwordPanel.PerformLayout();
+            this.loginBusttonPanel.ResumeLayout(false);
+            this.loginBusttonPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.PictureBox loginPictureBox;
+        private System.Windows.Forms.Panel usernamePanel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Panel passwordPanel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Panel loginBusttonPanel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button minimizeButton;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
